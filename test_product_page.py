a8ee1a9048df82172b29f120192d1d9a1ec4f22c
Igor Saigafarov <al-kopone@bk.ru>
2023-03-20 21:21:28 +0300

    pytest.ini add two mark, test_product_page.py add mark need_review

--- test_product_page.py
+++ test_product_page.py
@@ -26,6 +26,7 @@ class TestUserAddToBasketFromProductPage():
 		page.open()
 		page.should_not_be_success_message()
 
+	@pytest.mark.need_review
 	def test_user_can_add_product_to_basket(self, browser):
 		link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0"
 		page = ProductPage(browser, link)
@@ -36,7 +37,7 @@ class TestUserAddToBasketFromProductPage():
 		page.should_be_same_product_name_on_main_page()
 		page.should_be_same_product_price_on_main_page()
 
-
+@pytest.mark.need_review
 @pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
@@ -84,12 +85,14 @@ def test_guest_should_see_login_link_on_product_page(browser):
 	page.open()
 	page.should_be_login_link()
 
+@pytest.mark.need_review
 def test_guest_can_go_to_login_page_from_product_page(browser):
 	link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
 	page = ProductPage(browser, link)
 	page.open()
 	page.go_to_login_page()
 
+@pytest.mark.need_review
 def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
 	link = "http://selenium1py.pythonanywhere.com/ru/"
 	page = BasketPage(browser, link)
